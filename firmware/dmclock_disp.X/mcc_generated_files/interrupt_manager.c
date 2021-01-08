ec7cb2e271a953611b6f98eee188234cfd081a18
ikubaku <hide4d51@gmail.com>
2021-01-08 16:56:28 +0900

    Reconfigure EUSART1

--- firmware/dmclock_disp.X/mcc_generated_files/interrupt_manager.c
+++ firmware/dmclock_disp.X/mcc_generated_files/interrupt_manager.c
@@ -64,7 +64,15 @@ void __interrupt() INTERRUPT_InterruptManager (void)
     }
     else if(INTCONbits.PEIE == 1)
     {
-        if(PIE4bits.TMR3IE == 1 && PIR4bits.TMR3IF == 1)
+        if(PIE3bits.TX1IE == 1 && PIR3bits.TX1IF == 1)
+        {
+            EUSART1_TxDefaultInterruptHandler();
+        } 
+        else if(PIE3bits.RC1IE == 1 && PIR3bits.RC1IF == 1)
+        {
+            EUSART1_RxDefaultInterruptHandler();
+        } 
+        else if(PIE4bits.TMR3IE == 1 && PIR4bits.TMR3IF == 1)
         {
             TMR3_ISR();
         } 
