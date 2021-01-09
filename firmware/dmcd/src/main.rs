e1432c194282cbc392790e97d790e11446ae9ec4
ikubaku <hide4d51@gmail.com>
2021-01-09 23:13:08 +0900

    Improve the task system

--- firmware/dmcd/src/main.rs
+++ firmware/dmcd/src/main.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 use std::thread;
 use std::time::Duration;
 use std::sync::{Arc, Mutex};
+use std::sync::mpsc;
 
 use rppal::uart::{Parity, Uart};
 use rppal::i2c::I2c;
@@ -33,34 +34,60 @@ struct DMCState {
     ambient_temperature: f32,
 }
 
-fn create_clock_job(uart: Arc<Mutex<Uart>>) -> Box<dyn FnMut() + Send> {
-    Box::new(move || {
-        let mut uart_handle = uart.try_lock().unwrap();
-        let localtime = Local::now();
-        uart_handle.write(format!("T{}\r\n", localtime.format("%H%M")).as_bytes()).unwrap();
-    })
-}
-
-fn create_colon_blink_job(uart: Arc<Mutex<Uart>>) -> Box<dyn FnMut() + Send> {
-    Box::new(move || {
-        let mut uart_handle = uart.try_lock().unwrap();
-        uart_handle.write("A\r\n".as_bytes()).unwrap();
-        thread::sleep(Duration::from_millis(500));
-        uart_handle.write("D\r\n".as_bytes()).unwrap();
-    })
-}
-
-fn create_display_job<I2C>(i2c: &BusManagerStd<I2C>, state: Arc<Mutex<DMCState>>) -> Box<dyn FnMut() + Send + '_>
-    where I2C: embedded_hal::blocking::i2c::Write + Send,
-          <I2C as embedded_hal::blocking::i2c::Write>::Error : std::fmt::Debug,
-{
+fn main() -> Result<(), Box<dyn Error>> {
+    // Initialize hardware access
+    let uart = Uart::new(115200, Parity::None, 8, 1)?;
+    let uart = Arc::new(Mutex::new(uart));
+    let i2c = I2c::new()?;
+    let i2c: &'static _ = shared_bus::new_std!(I2c = i2c).unwrap();
     let mut display: GraphicsMode<_> = Builder::new().with_rotation(Rotate90).connect_i2c(i2c.acquire_i2c()).into();
     display.init().unwrap();
     display.flush().unwrap();
+    let mut bme280 = BME280::new_primary(i2c.acquire_i2c(), Delay);
+    bme280.init().unwrap();
 
-    Box::new(move || {
+    // Initialize other states
+    let state = DMCState::default();
+    let state = Arc::new(Mutex::new(state));
+
+    // Initialize scheduler
+    let mut scheduler = Scheduler::new();
+
+    // Create notifiers
+    let (clock_job_notifier_tx, clock_job_notifier_rx) = mpsc::channel();
+    let (colon_blink_job_notifier_tx, colon_blink_job_notifier_rx) = mpsc::channel();
+    let (display_job_notifier_tx, display_job_notifier_rx) = mpsc::channel();
+    let (sensor_job_notifier_tx, sensor_job_notifier_rx) = mpsc::channel();
+
+    // Create jobs
+    // clock
+    let uart_proxy_clock = uart.clone();
+    thread::spawn(move || {
+        clock_job_notifier_rx.recv().unwrap();
+
+        let mut uart_handle = uart_proxy_clock.try_lock().unwrap();
         let localtime = Local::now();
-        let state = state.try_lock().unwrap();
+        uart_handle.write(format!("T{}\r\n", localtime.format("%H%M")).as_bytes()).unwrap();
+    });
+
+    // blink colon
+    let uart_proxy_blink_colon = uart.clone();
+    thread::spawn(move || {
+        colon_blink_job_notifier_rx.recv().unwrap();
+
+        let mut uart_handle = uart_proxy_blink_colon.try_lock().unwrap();
+        uart_handle.write("A\r\n".as_bytes()).unwrap();
+        thread::sleep(Duration::from_millis(500));
+        uart_handle.write("D\r\n".as_bytes()).unwrap();
+    });
+
+    // display
+    let state_proxy_display = state.clone();
+    thread::spawn(move || {
+        display_job_notifier_rx.recv().unwrap();
+
+        let localtime = Local::now();
+        let state = state_proxy_display.try_lock().unwrap();
         display.clear();
         Text::new(localtime.format("%S").to_string().as_str(), Point::new(0, 96))
             .into_styled(TextStyle::new(Font24x32, BinaryColor::On))
@@ -71,46 +98,23 @@ fn create_display_job<I2C>(i2c: &BusManagerStd<I2C>, state: Arc<Mutex<DMCState>>
             .draw(&mut display)
             .unwrap();
         display.flush().unwrap();
-    })
-}
+    });
 
-fn create_sensor_job<I2C, E>(i2c: &BusManagerStd<I2C>, state: Arc<Mutex<DMCState>>) -> Box<dyn FnMut() + Send + '_>
-    where I2C: embedded_hal::blocking::i2c::Read<Error = E>
-    + embedded_hal::blocking::i2c::Write<Error = E>
-    + embedded_hal::blocking::i2c::WriteRead<Error = E>
-    + Send,
-          E: std::fmt::Debug,
-{
-    let mut bme280 = BME280::new_primary(i2c.acquire_i2c(), Delay);
-    bme280.init().unwrap();
+    // sensor
+    let state_proxy_sensor = state.clone();
+    thread::spawn(move || {
+        sensor_job_notifier_rx.recv().unwrap();
 
-    Box::new(move || {
         let bme280_res = bme280.measure().unwrap();
-        let mut state = state.try_lock().unwrap();
-
+        let mut state = state_proxy_sensor.try_lock().unwrap();
         state.ambient_temperature = bme280_res.temperature;
-    })
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    // Initialize hardware access
-    let uart = Uart::new(115200, Parity::None, 8, 1)?;
-    let uart = Arc::new(Mutex::new(uart));
-    let i2c = I2c::new()?;
-    let i2c: &'static _ = shared_bus::new_std!(I2c = i2c).unwrap();
-
-    // Initialize other states
-    let state = DMCState::default();
-    let state = Arc::new(Mutex::new(state));
-
-    // Initialize scheduler
-    let mut scheduler = Scheduler::new();
+    });
 
     // Assign periodic tasks
-    scheduler.every(5.minute()).run(create_sensor_job(i2c, state.clone()));
-    scheduler.every(1.minute()).run(create_clock_job(uart.clone()));
-    scheduler.every(1.second()).run(create_display_job(i2c, state.clone()));
-    scheduler.every(1.second()).run(create_colon_blink_job(uart.clone()));
+    scheduler.every(5.minute()).run(move || sensor_job_notifier_tx.send(()).unwrap());
+    scheduler.every(1.minute()).run(move || clock_job_notifier_tx.send(()).unwrap());
+    scheduler.every(1.second()).run(move || display_job_notifier_tx.send(()).unwrap());
+    scheduler.every(1.second()).run(move || colon_blink_job_notifier_tx.send(()).unwrap());
 
     // Do the main loop
     loop {
